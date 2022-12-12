166fa3970efa7f0324b0e5f1e2879840408c0d92
qucheng <qucheng@mail.ustc.edu.cn>
2022-12-12 11:22:07 +0800

    Remove env

--- env/__init__.py
+++ env/__init__.py
@@ -1,11 +1,6 @@
 import gym
 from gym.envs.registration import register
-from env.eth2 import Eth2, Eth2v1, Eth2v2, Eth2v3, Eth2v4, Eth2v5, Eth2v301, Eth2v302
-
-register(
-        id='eth2-v0',
-        entry_point='env:Eth2'
-)
+from env.eth2 import Eth2v1, Eth2v2
 
 register(
         id='eth2-v1',
@@ -15,29 +10,4 @@ register(
 register(
         id='eth2-v2',
         entry_point='env:Eth2v2'
-)
-
-register(
-        id='eth2-v3',
-        entry_point='env:Eth2v3'
-)
-
-register(
-        id='eth2-v301',
-        entry_point='env:Eth2v301'
-)
-
-register(
-        id='eth2-v302',
-        entry_point='env:Eth2v302'
-)
-
-register(
-        id='eth2-v4',
-        entry_point='env:Eth2v4'
-)
-
-register(
-        id='eth2-v5',
-        entry_point='env:Eth2v5'
 )
--- env/eth2v4/setup.py
+++
@@ -1,6 +0,0 @@
-#python setup.py build_ext --inplace
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy as np
-
-setup(name='eth2v4', ext_modules=cythonize("eth2v4.pyx"), include_dirs=[np.get_include()])
