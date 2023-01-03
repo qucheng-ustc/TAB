833dd43330d624e428a8722c143178708f56dc67
Qu Cheng <qucheng@mail.ustc.edu.cn>
2023-01-03 10:21:37 +0800

    add test file

---
+++ test_env.py
@@ -0,0 +1,14 @@
+from arrl.dataset import Dataset
+from env import Eth2v1
+
+if __name__=='__main__':
+    dataset = Dataset(start_time='2021-08-01 00:00:00')
+
+    env = Eth2v1(config=dict(txs=dataset.txs))
+
+    _, _, done, _ = env.reset()
+    while not done:
+        observation, reward, done, _ = env.step()
+    print(env.info())
+
+
