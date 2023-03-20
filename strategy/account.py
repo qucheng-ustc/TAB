de3b6541505aee38cda40054283cc9e0c602b0b9
Qu Cheng <qucheng@mail.ustc.edu.cn>
2023-03-20 21:50:14 +0800

    double

--- strategy/account.py
+++ strategy/account.py
@@ -45,3 +45,24 @@ class PartitionAccountAllocate(AccountAllocate):
                 return self.fallback.allocate(addr)
             else:
                 return -1
+
+# allocate a double account that contains two addr, allocate addr[0] first, if failed then allocate addr[1]
+class DoubleAccountAllocate(AccountAllocate):
+    def __init__(self, n_shards, base, fallback=None):
+        # base: allocate strategy to allocate each addr
+        # fallback: return fallback.allocate if failed to allocate
+        super().__init__(n_shards)
+        self.base = base
+        self.fallback = fallback
+    
+    def apply(self, action):
+        self.base.apply(action=action)
+
+    def allocate(self, addr):
+        ret = self.base.allocate(addr[0])
+        if ret!=-1:
+            return ret
+        ret = self.base.allocate(addr[1])
+        if ret!=-1:
+            return ret
+        return self.fallback.allocate(addr)
